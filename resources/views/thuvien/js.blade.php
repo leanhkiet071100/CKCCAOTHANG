446ade0180b25b8f5e2b03f7473ce67259ed93e3
leanhkiet071100 <leanhkiet071100@gmail.com>
2023-03-01 16:50:08 +0700

    upload

--- resources/views/thuvien/js.blade.php
+++ resources/views/thuvien/js.blade.php
@@ -1,4 +1,3 @@
     <script src="{{ url('js/main.min.js') }}"></script>
     <script src="{{ url('js/script.js') }}"></script>
     <script src="{{ url('js/map-init.js') }}"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8c55_YHLvDHGACkQscgbGLtLRdxBDCfI"></script>
